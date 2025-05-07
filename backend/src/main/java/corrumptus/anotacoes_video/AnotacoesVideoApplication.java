b3038b37ff0579ca3457e67955d04b6e2c7eec5f
corrumptus <lazarinilucas42@gmail.com>
2025-05-07 17:55:41 -0300

    feat: initiazling the backend folder with springboot initializr

---
+++ backend/src/main/java/corrumptus/anotacoes_video/AnotacoesVideoApplication.java
@@ -0,0 +1,13 @@
+package corrumptus.anotacoes_video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotacoesVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotacoesVideoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/corrumptus/anotacoes_video/AnotacoesVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package corrumptus.anotacoes_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotacoesVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
