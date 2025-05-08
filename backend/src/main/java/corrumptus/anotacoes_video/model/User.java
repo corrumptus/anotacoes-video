4ecf215e59dc1deaee9b5a76bdd7d2ffdff1c3eb
corrumptus <lazarinilucas42@gmail.com>
2025-05-07 23:43:51 -0300

    feat: creating a model for the user

---
+++ backend/src/main/java/corrumptus/anotacoes_video/model/User.java
@@ -0,0 +1,8 @@
+package corrumptus.anotacoes_video.model;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+    private String profilePicPath;
+}
