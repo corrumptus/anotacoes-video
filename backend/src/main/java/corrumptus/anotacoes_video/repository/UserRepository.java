f569c1042fcdad5e8681b078214a07c639827100
corrumptus <lazarinilucas42@gmail.com>
2025-05-09 20:11:20 -0300

    feat: creating the user repository

--- backend/src/main/java/corrumptus/anotacoes_video/repository/UserRepository.java
+++ backend/src/main/java/corrumptus/anotacoes_video/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package corrumptus.anotacoes_video.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import corrumptus.anotacoes_video.entity.UserEntity;
 
 public interface UserRepository extends Neo4jRepository<UserEntity, String> {
-    
+    @Query("""
+        MATCH (user:USER)
+        WHERE user.login = $login
+        RETURN user
+    """)
+    Optional<UserDetails> findByLogin(@Param("login") String login);
 }
