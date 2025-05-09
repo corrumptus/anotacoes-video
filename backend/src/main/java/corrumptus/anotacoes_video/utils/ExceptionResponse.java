7918de76a4d658b34d4d80b5050a6fe7536f86a9
corrumptus <lazarinilucas42@gmail.com>
2025-05-09 00:05:06 -0300

    feat: changing the exception response to a record

--- backend/src/main/java/corrumptus/anotacoes_video/utils/ExceptionResponse.java
+++ backend/src/main/java/corrumptus/anotacoes_video/utils/ExceptionResponse.java
@@ -1,9 +1,5 @@
 package corrumptus.anotacoes_video.utils;
 
-public class ExceptionResponse {
-    private String error;
-
-    public ExceptionResponse(String error) {
-        this.error = error;
-    }
+public record ExceptionResponse(String error) {
+    
 }
