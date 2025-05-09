cfab83f20542ddb4fcaaca0498c9df754b519363
corrumptus <lazarinilucas42@gmail.com>
2025-05-08 21:37:41 -0300

    feat: adding getters and constructors to user model

--- backend/src/main/java/corrumptus/anotacoes_video/model/User.java
+++ backend/src/main/java/corrumptus/anotacoes_video/model/User.java
@@ -1,8 +1,20 @@
 package corrumptus.anotacoes_video.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class User {
     private String id;
     private String name;
     private String password;
     private String profilePicPath;
+
+    public User(String name, String password, String profilePicPath) {
+        this.id = null;
+        this.name = name;
+        this.password = password;
+        this.profilePicPath = profilePicPath;
+    }
 }
