e82f7e3c7a72a3a8db60218ad7845f6f34681d3d
corrumptus <lazarinilucas42@gmail.com>
2025-05-09 20:10:59 -0300

    feat: turning the user entity into a spring user

--- backend/src/main/java/corrumptus/anotacoes_video/entity/UserEntity.java
+++ backend/src/main/java/corrumptus/anotacoes_video/entity/UserEntity.java
@@ -1,26 +1,69 @@
 package corrumptus.anotacoes_video.entity;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Node
+@Node("USER")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(generatorClass = UUIDStringGenerator.class)
     private String id;
-    
-    private String name;
+
+    private String login;
 
     private String password;
 
+    private String name;
+
     private String profilePicPath;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
