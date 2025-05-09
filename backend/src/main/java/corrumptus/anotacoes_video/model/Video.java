40b7625e27062bff1779dc3c58d7feb56b7653bc
corrumptus <lazarinilucas42@gmail.com>
2025-05-08 21:38:20 -0300

    feat: adding new properties, getters and constructors

--- backend/src/main/java/corrumptus/anotacoes_video/model/Video.java
+++ backend/src/main/java/corrumptus/anotacoes_video/model/Video.java
@@ -1,9 +1,26 @@
 package corrumptus.anotacoes_video.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Video {
-    private Long id;
+    private String id;
     private String path;
     private User owner;
+    private String title;
+    private String description;
     private VideoVisibility visibility;
     private boolean visitantCanAnotate;
+
+    public Video(String path, User owner, String title, String description) {
+        this.id = null;
+        this.path = path;
+        this.owner = owner;
+        this.title = title;
+        this.description = description;
+        this.visibility = VideoVisibility.PRIVATE;
+        this.visitantCanAnotate = false;
+    }
 }
