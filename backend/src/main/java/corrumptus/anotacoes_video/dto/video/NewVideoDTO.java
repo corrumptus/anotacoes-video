bb645cd07e7019d57dc7e418d4c179ed789aa329
corrumptus <lazarinilucas42@gmail.com>
2025-05-09 20:18:19 -0300

    feat: adding field validation for the new video dto

--- backend/src/main/java/corrumptus/anotacoes_video/dto/video/NewVideoDTO.java
+++ backend/src/main/java/corrumptus/anotacoes_video/dto/video/NewVideoDTO.java
@@ -2,10 +2,18 @@ package corrumptus.anotacoes_video.dto.video;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record NewVideoDTO(
     MultipartFile video,
+
+    @NotBlank
     String title,
+
+    @NotBlank
     String description,
+    
+    @NotBlank
     String ownerId
 ) {
     
