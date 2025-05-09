5aa6e96919010313b7449a89ab2b65ae41ddb956
corrumptus <lazarinilucas42@gmail.com>
2025-05-08 21:59:43 -0300

    feat: updating the video mapper to the new video dto

--- backend/src/main/java/corrumptus/anotacoes_video/mapper/VideoMapper.java
+++ backend/src/main/java/corrumptus/anotacoes_video/mapper/VideoMapper.java
@@ -1,5 +1,6 @@
 package corrumptus.anotacoes_video.mapper;
 
+import corrumptus.anotacoes_video.dto.video.VideoResponseDTO;
 import corrumptus.anotacoes_video.entity.VideoEntity;
 import corrumptus.anotacoes_video.model.Video;
 
@@ -27,6 +28,40 @@ public class VideoMapper {
             entity.getId(),
             entity.getPath(),
             UserMapper.toModel(entity.getOwner()),
+            entity.getTitle(),
+            entity.getDescription(),
+            entity.getVisibility(),
+            entity.isVisitantCanAnotate()
+        );
+    }
+
+    public static VideoResponseDTO toResponseFromModel(Video model) {
+        if (model == null)
+            return null;
+
+        return new VideoResponseDTO(
+            model.getId(),
+            model.getPath(),
+            model.getOwner().getId(),
+            model.getOwner().getName(),
+            model.getOwner().getProfilePicPath(),
+            model.getTitle(),
+            model.getDescription(),
+            model.getVisibility(),
+            model.isVisitantCanAnotate()
+        );
+    }
+    
+    public static VideoResponseDTO toResponseFromEntity(VideoEntity entity) {
+        if (entity == null)
+            return null;
+
+        return new VideoResponseDTO(
+            entity.getId(),
+            entity.getPath(),
+            entity.getOwner().getId(),
+            entity.getOwner().getName(),
+            entity.getOwner().getProfilePicPath(),
             entity.getTitle(),
             entity.getDescription(),
             entity.getVisibility(),
