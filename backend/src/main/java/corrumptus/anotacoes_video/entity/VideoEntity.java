d7fdbd04023b2f674ab6ac610b79e429c34194f8
corrumptus <lazarinilucas42@gmail.com>
2025-05-08 21:39:34 -0300

    feat: creating a video entity

---
+++ backend/src/main/java/corrumptus/anotacoes_video/entity/VideoEntity.java
@@ -0,0 +1,35 @@
+package corrumptus.anotacoes_video.entity;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import corrumptus.anotacoes_video.model.VideoVisibility;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Node
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoEntity {
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    private String id;
+
+    private String path;
+
+    @Relationship(value = "OWNS")
+    private UserEntity owner;
+
+    private String title;
+
+    private String description;
+
+    private VideoVisibility visibility;
+
+    private boolean visitantCanAnotate;
+}
