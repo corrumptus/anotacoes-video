b664ba93d6ea1242bdf396d150a0f040cf76f3b5
corrumptus <lazarinilucas42@gmail.com>
2025-07-25 18:48:42 -0300

    feat: adding a no args constructor, setting the id as a generated value and adding a update method to update the non-null fields incoming from the anotation update dto

--- backend/src/main/java/corrumptus/anotacoes_video/entity/Anotation.java
+++ backend/src/main/java/corrumptus/anotacoes_video/entity/Anotation.java
@@ -1,17 +1,23 @@
 package corrumptus.anotacoes_video.entity;
 
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.RelationshipId;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import corrumptus.anotacoes_video.dto.anotation.AnotationUpdateDTO;
 
 @RelationshipProperties
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class Anotation {
     @RelationshipId
+    @GeneratedValue
     private String id;
 
     private User user;
@@ -22,4 +28,14 @@ public class Anotation {
     private String anotation;
 
     private long videoInstant;
+
+    public void update(AnotationUpdateDTO dto) {
+        if (dto.anotation() != null) {
+            this.anotation = dto.anotation();
+        }
+
+        if (dto.videoInstant() != null) {
+            this.videoInstant = dto.videoInstant();
+        }
+    }
 }
